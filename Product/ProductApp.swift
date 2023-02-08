12ab61cefd212f74bb065acde97d3f12cbc7d166
Jelson J <50907905+jelsonjay@users.noreply.github.com>
2023-02-08 12:58:13 +0000

    Initial Commit

---
+++ Product/ProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductApp.swift
+//  Product
+//
+//  Created by Jelson J on 08/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
