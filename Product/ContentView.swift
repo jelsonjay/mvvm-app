9b0378331afce95123b626b619e6df9a2dd5d782
Jelson J <50907905+jelsonjay@users.noreply.github.com>
2023-02-09 08:53:40 +0000

    commit changes

--- Product/ContentView.swift
+++ Product/ContentView.swift
@@ -11,20 +11,35 @@ struct ContentView: View {
     @StateObject private var vm = ViewModel()
     var body: some View {
         VStack {
+            Text("Add a new Language")
             Toggle("Toggle Switch", isOn: $vm.isTurnedOn)
+                .padding()
+            
+            
+            HStack{
+                Button("Add"){
+                    vm.increment()
+                }
+                Text("\(vm.counter)")
+                    .bold()
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            List(vm.itemList){ item in
+                HStack{
+                    Text(item.name)
+                    Spacer()
+                    Text(item.desc)
+                }
+            }
+            .listStyle(.plain)
+            .background(.thinMaterial)
+            
+            Button("Add Item"){
+                vm.addItem()
+            }
             .padding()
         }
-        
-        HStack{
-            Button("Add"){
-                vm.increment()
-            }
-            Text("\(vm.counter)")
-                .bold()
-                .foregroundColor(.green)
-                .padding()
-        }
-       
     }
 }
 
